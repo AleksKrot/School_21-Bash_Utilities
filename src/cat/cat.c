896d1f00a23b958c2fd6ce733fdac559b4690388
Judson Clymene <judsoncl@student.21-school.ru>
2025-11-12 20:51:41 +0300

    develop: Moving print function to main.

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -1,8 +1,15 @@
 #include "cat_function.h"
 
 int main(const int argc, char *argv[]) {
+  bool error = EXIT_SUCCESS;
   opterr = 0;
   Flags flags;
-  bool error = process_files(argc, argv, &flags);
+  init_flags(&flags);
+  error = process_files(argc, argv, &flags);
+  for (int i = 0; i < argc; i++) {
+    if (print_file(argv[i], &flags) != 0) {
+      error = true;
+    }
+  }
   return error;
 }
