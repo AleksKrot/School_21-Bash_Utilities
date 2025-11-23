2ec32ba97de8e3299cddbc57a6e1ec1c8d50172a
Александра Крот <aleksandra.krot88@gmail.com>
2025-11-23 22:14:10 +0300

    Delete src/cat directory

--- src/cat/cat.c
+++
@@ -1,9 +0,0 @@
-#include "../common/common_function.h"
-#include "cat_function.h"
-
-int main(int argc, char *argv[]) {
-  opterr = 0;
-  Flags flags;
-  bool error = process_files(argc, argv, &flags);
-  return error;
-}
